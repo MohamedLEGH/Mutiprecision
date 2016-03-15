35e52ac352472c1f20a36fed7c79628e6aac4afe
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2016-03-15 16:01:42 +0100

    Update vectorization.py
    j'ai juste fait a sous la forme d'un vecteur colonne au lieu d'un vecteur ligne

--- vectorization.py
+++ vectorization.py
@@ -9,12 +9,13 @@
 import python-flint 
 
 def vec(A):
-"""
-Ce programme permet de vectorizer une matrice : On transforme une matrice n*m sous la forme d'un vecteur n*m colonne
-
-"""
+	
+	"""
+	Ce programme permet de vectorizer une matrice : On transforme une matrice n*m sous la forme d'un vecteur n*m colonne 
+	dans le style FORTRAN.
+	"""
 	k=0 # l'indice pour notre vecteur
-	a=arb_mat(1,A.ncols*A.nrows) # vecteur (matrice 1xtaille(A) ) rempli par des zéros 
+	a=arb_mat(A.ncols*A.nrows,1) # vecteur (matrice taille(A)*1 ) rempli avec des zéros 
 	for j in A.ncols:
 		for i in A.nrows:
 			a[k]=A[i,j]
